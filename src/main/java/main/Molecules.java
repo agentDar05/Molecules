049507b9d4c7fc7bffacc78ac98cd0e9449654fa
Darina Tkachenko <potterpeppi@gmail.com>
2025-12-01 19:06:55 +0200

    Add methods to calculate weight of molecules

--- src/main/java/main/Molecules.java
+++ src/main/java/main/Molecules.java
@@ -12,8 +12,6 @@ public class Molecules {
         if (!Utils.isValid(molecule)){
             return;
         }
-        storage.clear();
-        offset.clear();
         int i = 0;
         while (i < molecule.length()) {
             char c = molecule.charAt(i);
@@ -86,17 +84,19 @@ public class Molecules {
             }
             if (number == 0) number = 1;
         }
-        Integer numberInPTable = Utils.numberInPTable(name);
-        if (numberInPTable == null || numberInPTable < 0) {
-            return;
-        }
+        int numberInPTable = Utils.numberInPTable(name);
         Integer foundNumber = storage.get(numberInPTable);
         if (foundNumber != null) {
             number += foundNumber;
         }
         storage.put(numberInPTable, number);
     }
-
+    public double calculateMW(){
+        return Utils.calculateMW(storage);
+    }
+    public double calculateEMW(){
+        return Utils.calculateEMW(storage);
+    }
     @Override
     public String toString() {
         if (storage.isEmpty()) return "";
@@ -141,6 +141,44 @@ public class Molecules {
                 "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
                 "Rg", "Cn", "Nh", "Fl", "Mc"
         };
+
+        final static double[] MW = new double[]{
+                1.008, 4.0026,
+                6.94, 9.0122,
+                10.81, 12.011, 14.007, 15.999, 18.998, 20.180,
+                22.990, 24.305, 26.982, 28.085, 30.974, 32.06, 35.45, 39.948,
+                39.098, 40.078, 44.956, 47.867, 50.942, 51.996, 54.938, 55.845, 58.933, 58.693,
+                63.546, 65.38, 69.723, 72.630, 74.922, 78.971, 79.904, 83.798,
+                85.468, 87.62, 88.906, 91.224, 92.906, 95.95, 98.0, 101.07, 102.91, 106.42,
+                107.87, 112.41, 114.82, 118.71, 121.76, 127.60, 126.90, 131.29,
+                132.91, 137.33,
+                138.91, 140.12, 140.91, 144.24,
+                145.0, 150.36, 151.96, 157.25, 158.93, 162.50, 164.93, 167.26, 168.93, 173.05,
+                174.97, 178.49, 180.95, 183.84, 186.21, 190.23, 192.22, 195.08, 196.97, 200.59,
+                204.38, 207.2, 208.98, 209.0, 210.0, 222.0, 223.0, 226.0, 227.0,
+                232.04, 231.04, 238.03, 237.0, 244.0,
+                243.0, 247.0, 247.0, 251.0, 252.0, 257.0, 258.0, 259.0,
+                262.0, 267.0, 268.0, 271.0, 270.0, 277.0, 278.0, 281.0, 282.0, 285.0, 286.0,
+                289.0, 290.0, 293.0, 294.0, 294.0
+        };
+        final static double[] MONO = new double[]{
+                1.00782503223, 4.00260325413, 7.0160034366, 9.012183065, 11.00930536, 12.00000000000, 14.00307400443, 15.99491461957,
+                18.998403163, 19.9924401762, 22.9897692820, 23.985041697, 26.981538626, 27.9769265325, 30.97376199842, 31.9720711744,
+                34.968852682, 39.9623831237, 38.9637064864, 39.962590863, 44.95590828, 47.94794628, 50.943959507, 51.94050623,
+                54.93804391, 55.93493633, 58.93319429, 57.93534241, 62.92959772, 63.92914201, 68.9255735, 73.92117776,
+                74.92159457, 78.9183376, 79.9165218, 82.91413012, 84.9117897379, 87.905612124, 88.9058403, 89.9046977,
+                92.9063730, 97.90540482, 98.0, 101.904349, 102.905504, 105.903480, 106.905097, 113.903365,
+                114.903878, 119.902202, 120.9038157, 126.904473, 131.9041535, 132.90545196, 137.9052472, 138.9063563,
+                139.9054431, 140.9076576, 141.907729, 144.912749, 151.919739, 152.921238, 157.924112, 158.9253547,
+                163.9291819, 164.9303288, 165.9302995, 168.9342179, 173.9388664, 174.9407752, 179.9465570, 180.947996,
+                183.950930, 186.955750, 189.959217, 192.962982, 194.964774, 196.966569, 201.970643, 204.9744275,
+                207.9766521, 208.9803991, 208.9824308, 209.9871479, 222.0175777, 223.019736, 226.0254098, 227.0277523,
+                232.0380558, 231.03588, 238.0507882, 237.0481736, 244.064205, 243.0613813, 247.070307, 247.070299,
+                251.0795886, 252.08298, 257.095106, 258.098431, 259.10103, 262.10961, 267.0, 268.0,
+                271.0, 270.0, 276.0, 277.0, 281.0, 282.0, 285.0, 286.0,
+                289.0, 290.0, 293.0, 294.0, 294.0
+        };
+
         private static final Map<String, Integer> PT_MAP = new HashMap<>();
 
         static {
@@ -158,8 +196,32 @@ public class Molecules {
         public static boolean existsInPTable(String s) {
             return PT_MAP.containsKey(s); // O(1)
         }
-        public static Integer numberInPTable(String s) {
-            return PT_MAP.getOrDefault(s, -1); // O(1)
+        public static int numberInPTable(String s) {
+            Integer e = PT_MAP.get(s);// O(1)
+            if (e == null)
+                throw new IllegalArgumentException("No such chemical element: " + s);
+            return e;
+
+        }
+        public static double calculateMW(Map<Integer, Integer> elementsInMap) {
+            double mw = 0.0;
+            for (Map.Entry<Integer, Integer> element : elementsInMap.entrySet()) {
+                int key = element.getKey();
+                int value = element.getValue();
+                double m = Utils.MW[key];
+                mw += m * value;
+            }
+            return mw;
+        }
+        public static double calculateEMW(Map<Integer, Integer> elementsInMap) {
+            double mw = 0.0;
+            for (Map.Entry<Integer, Integer> element : elementsInMap.entrySet()) {
+                int key = element.getKey();
+                int value = element.getValue();
+                double m = Utils.MONO[key];
+                mw += m * value;
+            }
+            return mw;
         }
         public static boolean isValid(String s) {
             int count = 0;
