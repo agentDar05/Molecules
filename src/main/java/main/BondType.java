e7c79bb19b615ea9bf107459e1667f9a4324fa33
dtkachenko <potterpeppi@gmail.com>
2026-05-06 12:58:37 +0200

    Add more bond types

--- src/main/java/main/BondType.java
+++ src/main/java/main/BondType.java
@@ -3,4 +3,10 @@ package main;
 public class BondType {
     public static final byte SINGLE = 1;
     public static final byte DOUBLE = 2;
+    public static final byte TRIPLE = 3;
+    public static final byte QUADRUPLE = 4;
+    public static final byte AROMATIC = 5;
+    public static final byte COORDINATE = 6;
+    public static final byte ANY = 7;
+
 }
