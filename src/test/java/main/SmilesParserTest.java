ce9e9323cbc9691ed1853a8017e65ea10401f99b
dtkachenko <potterpeppi@gmail.com>
2026-07-09 18:38:40 +0200

    Add tests

--- src/test/java/main/SmilesParserTest.java
+++ src/test/java/main/SmilesParserTest.java
@@ -4,42 +4,47 @@ import main.VF2.MoleculeWithAdjacencyList;
 import org.junit.jupiter.api.Test;
 
 public class SmilesParserTest {
+    private static final byte C = 6;
+    private static final byte Cl = 17;
+    private static final byte Br = 18;
+
+
     @Test
     void parseAtoms() {
         SmilesParser.read("CCC");
         MoleculeWithAdjacencyList m = new MoleculeWithAdjacencyList();
-        m.addAtom((byte)6);
-        m.addAtom((byte)6);
-        m.addAtom((byte)6);
+        m.addAtom(C);
+        m.addAtom(C);
+        m.addAtom(C);
     }
     @Test
     void parseTwoLetterAtoms() {
         SmilesParser.read("ClBrCl");
         MoleculeWithAdjacencyList m = new MoleculeWithAdjacencyList();
-        m.addAtom((byte)17);
-        m.addAtom((byte)35);
-        m.addAtom((byte)17);
+        m.addAtom(Cl);
+        m.addAtom(Br);
+        m.addAtom(Cl);
     }
     @Test
     void parseSingleBonds() {
         SmilesParser.read("C-Cl-Br-C");
         MoleculeWithAdjacencyList m = new MoleculeWithAdjacencyList();
-        m.addAtom((byte)6);
-        m.addAtom((byte)17);
-        m.addAtom((byte)35);
-        m.addAtom((byte)6);
+        m.addAtom(C);
+        m.addAtom(Cl);
+        m.addAtom(Br);
+        m.addAtom(C);
     }
     @Test
     void parseDoubleBonds() {
         SmilesParser.read("C=Cl=Br=C");
         MoleculeWithAdjacencyList m = new MoleculeWithAdjacencyList();
-        m.addAtom((byte)6);
-        m.addAtom((byte)17);
-        m.addAtom((byte)35);
-        m.addAtom((byte)6);
+        m.addAtom(C);
+        m.addAtom(Cl);
+        m.addAtom(Br);
+        m.addAtom(C);
         m.addBond(0, 1, BondType.DOUBLE);
-        m.addBond(0, 1, BondType.DOUBLE);
-        m.addBond(0, 1, BondType.DOUBLE);
+        m.addBond(1, 2, BondType.DOUBLE);
+        m.addBond(2, 3, BondType.DOUBLE);
 
 
     }
@@ -51,4 +56,21 @@ public class SmilesParserTest {
     void parseDifferentBonds() {
         SmilesParser.read("C-Cl=Br#C");
     }
+    @Test
+    void parseBranchedStructure(){
+        MoleculeWithAdjacencyList m = new MoleculeWithAdjacencyList();
+        m.addAtom(C); // 0
+        m.addAtom(C); // 1
+        m.addAtom(C); // 2
+        m.addAtom(C); // 3
+        m.addAtom(C); // 4
+        m.addAtom(C); // 5
+        m.addBond(0, 1, BondType.SINGLE);
+        m.addBond(0, 2, BondType.SINGLE);
+        m.addBond(2, 3, BondType.SINGLE);
+        m.addBond(3, 4, BondType.SINGLE);
+        m.addBond(4, 5, BondType.SINGLE);
+        Parser.compareMolecules(SmilesParser.read("C(C)CCCC"), m);
+    }
+
 }
